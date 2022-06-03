ca21e5870983abdb3ae6d99668df0c65b6423197
ElYizuz123 <106762901+ElYizuz123@users.noreply.github.com>
2022-06-02 19:31:18 -0500

    Update Interface.java

--- Interface.java
+++ Interface.java
@@ -1,8 +1,5 @@
 public interface Interface{
 	public void mostrar();
 	public void capturar();
-	public String queSoy();
-	
-	System.out.println("Esetera esetera esetera esetera ezetera");
-	
+	public String queSoy();	
 }
