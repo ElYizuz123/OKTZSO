a83aba24b51cd3ef3921d8e4a9b8dc69868998b3
Guty90 <alexyanguie5@gmail.com>
2022-06-05 14:54:56 -0500

    Create DetalleC.java

---
+++ DetalleC.java
@@ -0,0 +1,51 @@
+import java.util.Scanner;
+public class DetalleC implements Interface{
+	private String idProducto;
+	private int cantidad;
+	private double costo;
+
+	Inventario inv=New Inventario();
+
+	public DetalleC(String idProducto, int cantidad, double costo){
+		this.idProducto=idProducto;
+		this.cantidad=cantidad;
+		this.costo=costo;
+	}
+	public String queSoy(){
+		return "DetalleC";
+	}
+
+  public void mostrar(){}
+
+	public void capturar(){
+		Scanner leer=new Scanner(System.in);
+		int opcion;
+		System.out.println("----------CAPTURA LA COMPRA----------");
+		System.out.print("1.- AGREGAR PRODUCTO NUEVO      2.-AGREGAR PRODUCTO EXISTENTE");
+		opcion=leer.nextInt();
+
+		if(opcion==1)
+			inv.capturarNuevoProducto();
+		else
+			inv.sumarProductos();
+	}
+
+	public String getIdProducto(){
+		return idProducto;
+	}
+	public void setIdProducto(String idProducto){
+		this.idProducto=idProducto;
+	}
+	public int getCantidad(){
+		return cantidad;
+	}
+	public void setCantidad(int cantidad){
+		this.cantidad=cantidad;
+	}
+	public double getCosto(){
+		return costo;
+	}
+	public void setCosto(int costo){
+		this.costo=costo;
+	}
+}
