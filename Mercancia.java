f5ab045423f22874e5e301ea257c5a934907f33f
Guty90 <alexyanguie5@gmail.com>
2022-06-05 15:35:52 -0500

    Update Mercancia.java

--- Mercancia.java
+++ Mercancia.java
@@ -5,9 +5,9 @@ public class Mercancia{
 	private double costo;
 	private double precio;
 	private String nombreProducto;
-	
+
 	public Mercancia(){
-		
+
 	}
 	public Mercancia(String idProducto, int cantidad, double costo, double precio, String nombreProducto){
 		this.idProducto=idProducto;
@@ -16,15 +16,15 @@ public class Mercancia{
 		this.precio=precio;
 		this.nombreProducto=nombreProducto;
 	}
-	
+
 	public void mostrar(){
 		System.out.println("Productos:");
 		System.out.println("ID del producto: " +idProducto);
 		System.out.println("Nombre del producto: "+nombreProducto);
 		System.out.println("Cantidad: "+cantidad);
-		
+
 	}
-	
+
 	public void capturar(){
 		Scanner scn = new Scanner (System.in);
 		System.out.println("---------------CAPTURAR PRODUCTO----------------");
@@ -32,10 +32,9 @@ public class Mercancia{
 		System.out.println("Inserte el nombre del producto: "); nombreProducto=scn.nextLine();
 		System.out.println("Inserte el costo individual del producto: ");costo=scn.nextDouble();
 		System.out.println("Inserte el precio al publico: ");precio=scn.nextDouble();
-		
 	}
-	
-	
+
+
 	public String getNombreProducto(){
 		return nombreProducto;
 	}
@@ -60,11 +59,11 @@ public class Mercancia{
 	public String getIdProducto(){
 		return idProducto;
 	}
-	
+
 	public void setCantidad(int cantidad){
 		this.cantidad=cantidad;
 	}
 	public int getCantidad(){
 		return cantidad;
-	}	
+	}
 }
