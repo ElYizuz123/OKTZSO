adcaf5f484453e03c666c3dd531bdd779c19350c
Guty90 <alexyanguie5@gmail.com>
2022-06-05 13:22:24 -0500

    Update Inventario.java

--- Inventario.java
+++ Inventario.java
@@ -1,44 +1,44 @@
 import java.util.Scanner;
-public class Inventario implements interface{
+public class Inventario implements Interface{
 	private String idProducto;
 	private int cantidad;
-	
+
 	public Inventario (String idProducto, int cantidad){
 		this.idProducto=idProducto;
 		this.cantidad=cantidad;
 	}
-	
-	public Inventario{
-		
+
+	public Inventario(){
+
 	}
-	
+
 	public void mostrar(){
 		System.out.println("Inventario:");
 		System.out.println("ID del producto: " +idProducto);
 		System.out.println("Cantidad: "+cantidad);
 	}
-	
+
 	public void capturar(){
 		Scanner scn = new Scanner (System.in);
 		System.out.println("Capturar Inventario:");
 		System.out.println("ID del producto: "); idProducto=scn.nextLine();
 		System.out.println("Cantidad: "); cantidad=scn.nextInt();
 	}
-	
-	
-	
-	public Void setIdProducto(String idProducto){
+
+
+
+	public void setIdProducto(String idProducto){
 		this.idProducto=idProducto;
 	}
 	public String getIdProducto(){
 		return idProducto;
 	}
-	
-	public Void setCantidad(int cantidad){
+
+	public void setCantidad(int cantidad){
 		this.cantidad=cantidad;
 	}
 	public int getCantidad(){
 		return cantidad;
-	}	
-	
+	}
+
 }
